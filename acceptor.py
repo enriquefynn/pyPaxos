12215caf0a82073a56095122276448aa659a6e29
TitusCornelius <titus@vonkoeller.com>
2015-12-08 08:01:42 +0100

    use named tuple to track instance state

--- acceptor.py
+++ acceptor.py
@@ -6,6 +6,11 @@ import sys
 from message_pb2 import Message
 from entity import Entity
 
+from collections import namedtuple
+class InstanceState(namedtuple('InstanceState', 'ballot vballot vmsg')):
+    def __new__(cls, ballot=None, vballot=-1, vmsg=''):
+        return super(InstanceState, cls).__new__(cls, ballot, vballot, vmsg)
+
 from logger import get_logger
 from sys import argv
 critical, info, debug = get_logger(__name__, argv)
@@ -25,41 +30,37 @@ class Acceptor(Entity):
             if parsed_message.type == Message.PHASE1A:
                 if not parsed_message.instance in self.instance:
                     #Init with null
-                    self.instance[parsed_message.instance] = (parsed_message.ballot, -1, '')
-                if parsed_message.ballot >= self.instance[parsed_message.instance][0]:
-                    self.instance[parsed_message.instance] = (parsed_message.ballot,
-                    self.instance[parsed_message.instance][1],
-                    self.instance[parsed_message.instance][2])
+                    self.instance[parsed_message.instance] = InstanceState(parsed_message.ballot)
+                if parsed_message.ballot >= self.instance[parsed_message.instance].ballot:
+                    self.instance[parsed_message.instance] = self.instance[parsed_message.instance]._replace(ballot=parsed_message.ballot)                   
                     message = Message(type = Message.PHASE1B,
-                                                  id = self._id,
-                                                  instance = parsed_message.instance,
-                                                  ballot = self.instance[parsed_message.instance][0],
-                                                  vballot = self.instance[parsed_message.instance][1],
-                                                  vmsg = self.instance[parsed_message.instance][2])
+                                      id = self._id,
+                                      instance = parsed_message.instance,
+                                      **self.instance[parsed_message.instance]._asdict())
                     self.send(message, 'proposers')
                 else: #We require a higher ballot
                     message = Message(instance = parsed_message.instance,
-                                                  type = Message.HIGHBAL,
-                                                  id = self._id)
+                                      type = Message.HIGHBAL,
+                                      id = self._id)
                     self.send(message, 'proposers')
                 
             elif parsed_message.type == Message.PHASE2A:
                 debug('Received decide')
                 debug(parsed_message)
                 debug(self.instance[parsed_message.instance])
-                debug('EAAAA1 %s %s %s', parsed_message.ballot, self.instance[parsed_message.instance][0], self.instance[parsed_message.instance][1])
-                if (parsed_message.ballot >= self.instance[parsed_message.instance][0] and
-                    parsed_message.ballot != self.instance[parsed_message.instance][1]):
-                    self.instance[parsed_message.instance] = (parsed_message.ballot, 
-                    parsed_message.ballot,
-                    parsed_message.msg)
+                debug('EAAAA1 %s %s %s', parsed_message.ballot, self.instance[parsed_message.instance])
+                if (parsed_message.ballot >= self.instance[parsed_message.instance].ballot and
+                    parsed_message.ballot != self.instance[parsed_message.instance].vballot):
+                    self.instance[parsed_message.instance] = InstanceState(parsed_message.ballot, 
+                                                                           parsed_message.ballot,
+                                                                           parsed_message.msg)
                     message = Message(instance = parsed_message.instance,
-                                                  ballot = parsed_message.ballot,
-                                                  type = Message.PHASE2B,
-                                                  id = self._id,
-                                                  msg = parsed_message.msg)
+                                      ballot = parsed_message.ballot,
+                                      type = Message.PHASE2B,
+                                      id = self._id,
+                                      msg = parsed_message.msg)
                     self.send(message, 'proposers')
-                debug('EAAAA2 %s %s %s', parsed_message.ballot, self.instance[parsed_message.instance][0], self.instance[parsed_message.instance][1])
+                debug('EAAAA2 %s %s %s', parsed_message.ballot, self.instance[parsed_message.instance])
 
 
 if __name__ == '__main__':
