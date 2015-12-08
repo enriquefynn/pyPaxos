a6529a8377524d39c2d2ad9be1eea6ce975ff8c1
TitusCornelius <titus@vonkoeller.com>
2015-12-08 08:41:36 +0100

    use named tuple for instance state

--- proposer.py
+++ proposer.py
@@ -7,6 +7,11 @@ import sys
 from message_pb2 import Message
 from entity import Entity
 
+from collections import namedtuple
+class InstanceState(namedtuple('InstanceState', 'ballot acceptor_messages timestamp phase msg')):
+    def __new__(cls, ballot, timestamp, phase, msg):
+        return super(InstanceState, cls).__new__(cls, ballot, [], timestamp, phase, msg)   
+
 from logger import get_logger
 from sys import argv
 critical, info, debug = get_logger(__name__, argv)
@@ -33,24 +38,21 @@ class Proposer(Entity):
             #Got a Proposal from client
             if parsed_message.type == Message.PROPOSAL:
                 debug(parsed_message)
-                self.state[self.instance] = {
-                'ballot': self._id,
-                'acceptor_messages': [],
-                'timestamp': time.time(),
-                'phase': Message.PHASE1A,
-                'msg': parsed_message.msg
-                }
+                self.state[self.instance] = InstanceState(ballot = self._id,
+                                                          timestamp = time.time(),
+                                                          phase = Message.PHASE1A,
+                                                          msg = parsed_message.msg)
                 #Build Phase1A
                 message = Message(type = Message.PHASE1A,
                                               id = self._id)
                 #learner catch up msg
                 if parsed_message.instance != -1:
                     message.instance = parsed_message.instance
-                    self.state[parsed_message.instance]['ballot']+=100
-                    message.ballot = self.state[parsed_message.instance]['ballot']
+                    message.ballot = self.state[parsed_message.instance].ballot +100
+                    self.state[parsed_message.instance] = self.state[parsed_message.instance]._replace(ballot = message.ballot)
                 #client instance
                 else:
-                    message.ballot = self.state[self.instance]['ballot']
+                    message.ballot = self.state[self.instance].ballot
                     message.instance = self.instance
                     self.instance+=1
                 self.send(message, 'acceptors')
@@ -63,16 +65,15 @@ class Proposer(Entity):
                 else:
                     self.acceptor_messages[parsed_message.instance].append(parsed_message)
                 #Already sent 2A (had a quorum)
-                if self.state[parsed_message.instance]['phase'] == Message.PHASE2A:
+                if self.state[parsed_message.instance].phase == Message.PHASE2A:
                     continue
                 #See if quorum is reached
                 n_msgs = Set([])
-                current_propose = (-1, self.state[parsed_message.instance]['msg'])
+                current_propose = (-1, self.state[parsed_message.instance].msg)
                 debug(self.acceptor_messages)
                 for msg in self.acceptor_messages[parsed_message.instance]:
-                    debug('-----00-----')
                     debug(msg)
-                    if msg.ballot == self.state[parsed_message.instance]['ballot']:
+                    if msg.ballot == self.state[parsed_message.instance].ballot:
                         n_msgs.add(msg.id)
                         current_propose = max(current_propose, 
                         (parsed_message.vballot, parsed_message.vmsg))
@@ -80,13 +81,12 @@ class Proposer(Entity):
 
                 if len(n_msgs) >= (self.get_number_of_acceptors()+1)/2:
                     debug('Quorum reached, initiating 2A')
-                    self.state[parsed_message.instance]['phase'] = Message.PHASE2A
-                    self.state[parsed_message.instance]['timestamp'] = time.time()
+                    self.state[parsed_message.instance] = self.state[parsed_message.instance]._replace(phase = Message.PHASE2A, timestamp = time.time())
                     message = Message(type = Message.PHASE2A,
-                                                  id = self._id,
-                                                  ballot = self.state[parsed_message.instance]['ballot'],
-                                                  msg = current_propose[1],
-                                                  instance = parsed_message.instance)
+                                      id = self._id,
+                                      ballot = self.state[parsed_message.instance].ballot,
+                                      msg = current_propose[1],
+                                      instance = parsed_message.instance)
                     self.send(message, 'acceptors')
 
             #Received phase 2B
@@ -102,11 +102,12 @@ class Proposer(Entity):
                 
                 #Quorum reached, must inform learners
                 if (len(n_msgs) >= (self.get_number_of_acceptors()+1)/2 and
-                self.state[parsed_message.instance]['phase'] != Message.DECISION):
+                    self.state[parsed_message.instance].phase != Message.DECISION):
                     debug('Informing decide to learners')
                     debug(parsed_message)
-                    self.state[parsed_message.instance]['phase'] = Message.DECISION
-                    self.state[parsed_message.instance]['timestamp'] = time.time()
+
+                    self.state[parsed_message.instance] = self.state[parsed_message.instance]._replace(phase = Message.DECISION, timestamp = time.time())
+
                     message = Message(type = Message.DECISION,
                                                   id = self._id,
                                                   msg = parsed_message.msg,
@@ -119,14 +120,13 @@ class Proposer(Entity):
                 if not parsed_message.instance in self.state:
                     continue
                 #Grow ballot by arbitrary number
-                self.state[parsed_message.instance]['ballot'] += 100 
-                self.state[parsed_message.instance]['timestamp'] = time.time()
+                self.state[parsed_message.instance] = self.state[parsed_message.instance]._replace(ballot = self.state[parsed_message.instance].ballot +100, timestamp = time.time())
                 #Build Phase1A
                 #TODO: don't replicate code
                 message = Message(type = Message.PHASE1A,
                                               id = self._id,
                                               instance = parsed_message.instance,
-                                              ballot = self.state[parsed_message.instance]['ballot'])
+                                              ballot = self.state[parsed_message.instance].ballot)
                 self.send(message, 'acceptors')
     
     def check_unresponsive_msgs(self):
@@ -134,21 +134,19 @@ class Proposer(Entity):
         return 0
         while True:
             for instance in self.state:
-                if (self.state[instance]['phase'] != Message.DECISION and
-                time.time() - self.state[instance]['timestamp'] > self.get_timeout_msgs()):
+                if (self.state[instance].phase != Message.DECISION and
+                time.time() - self.state[instance].timestamp > self.get_timeout_msgs()):
                     debug('Found unresponsive messages, will try again')
 
                     #Grow ballot by arbitrary number
-                    self.state[instance]['ballot'] +=100
-                    self.state[instance]['timestamp'] = time.time()
-                    self.state[instance]['phase'] = Message.PHASE1A
+                    self.state[instance] = self.state[instance]._replace(ballot = self.state[instance] + 100, timestamp = time.time(), phase = Message.PHASE1A)
 
                     #Build Phase1A
                     #TODO: don't replicate code
                     message = Message(type = Message.PHASE1A,
                                                   id = self._id,
                                                   instance = instance,
-                                                  ballot = self.state[instance]['ballot'])
+                                                  ballot = self.state[instance].ballot)
                     debug('msg:')
                     debug(message)
                     self.send(message, 'acceptors')
