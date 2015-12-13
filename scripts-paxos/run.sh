4e85d8a42322fa8c4614a4c5a5963311719c89e4
TitusCornelius <titus@vonkoeller.com>
2015-12-13 18:40:35 +0100

    fixed run.sh

--- scripts-paxos/run.sh
+++ scripts-paxos/run.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 projdir="$1"
 conf=`pwd`/paxos.conf
 n="$2"
@@ -8,44 +9,36 @@ if [[ x$projdir == "x" || x$n == "x" ]]; then
     exit 1
 fi
 
-# following line kills processes that have the config file in its cmdline
-KILLCMD="pkill -f $conf"
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM exit
 
-$KILLCMD
-
-cd $projdir
-
-../generate.sh $n > ../prop1
-../generate.sh $n > ../prop2
+./generate.sh $n > prop1
+./generate.sh $n > prop2
 
 echo "starting acceptors..."
 
-./acceptor.sh 1 $conf &
-./acceptor.sh 2 $conf &
-./acceptor.sh 3 $conf &
+$projdir/acceptor.sh 1 $conf &
+$projdir/acceptor.sh 2 $conf &
+$projdir/acceptor.sh 3 $conf &
 
-sleep 1
+sleep .5
 echo "starting learners..."
 
-./learner.sh 1 $conf > ../learn1 &
-./learner.sh 2 $conf > ../learn2 &
+$projdir/learner.sh 1 -vvv $conf > learn1 &
+$projdir/learner.sh 2 -vvv $conf > learn2 &
 
-sleep 1
+sleep .5
 echo "starting proposers..."
 
-./proposer.sh 1 $conf &
-./proposer.sh 2 $conf &
+$projdir/proposer.sh 1 $conf &
+$projdir/proposer.sh 2 $conf &
 
 echo "waiting to start clients"
-sleep 10
+sleep .5
 echo "starting clients..."
 
-./client.sh 1 $conf < ../prop1 &
-./client.sh 2 $conf < ../prop2 &
+$projdir/client.sh 1 -vvv $conf < prop1 &
+$projdir/client.sh 2 -vvv $conf < prop2 &
 
 sleep 5
 
-$KILLCMD
 wait
-
-cd ..
