c45052ea1bda694135ea41bb53a2bcedc76f21a4
TitusCornelius <titus@vonkoeller.com>
2015-12-13 18:53:30 +0100

    explicitly flush output

--- learner.py
+++ learner.py
@@ -34,14 +34,16 @@ class Learner(Entity):
                             self.non_printed_instances[instance] != ""):
                                 info('Decided %s', parsed_message.msg)
                                 self.decided[parsed_message.instance] = parsed_message.msg
-                                print(parsed_message.msg)
+                                sys.stdout.write("%s\n" % parsed_message.msg)
+                                sys.stdout.flush()
                         self.non_printed_instances = {}
                         self.last_received_instance = self.maximum_instance
                     else:
                         self.last_received_instance = parsed_message.instance
                         self.maximum_instance = parsed_message.instance
                         info('Decided %s', parsed_message.msg)
-                        print(parsed_message.msg)
+                        sys.stdout.write("%s\n" % parsed_message.msg)
+                        sys.stdout.flush()
                         self.decided[parsed_message.instance] = parsed_message.msg
 
                 #Catch up
